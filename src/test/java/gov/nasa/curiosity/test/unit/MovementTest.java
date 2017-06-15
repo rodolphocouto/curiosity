55c2d72429466b06998d8348c33a699a03672e0e
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2017-06-15 18:19:08 -0300

    Implements unit tests for different movements scenarios

--- src/test/java/gov/nasa/curiosity/test/unit/MovementTest.java
+++ src/test/java/gov/nasa/curiosity/test/unit/MovementTest.java
@@ -1,7 +1,9 @@
 package gov.nasa.curiosity.test.unit;
 
 import gov.nasa.curiosity.domain.exception.UnrecognizedMovementException;
+import gov.nasa.curiosity.domain.model.Direction;
 import gov.nasa.curiosity.domain.model.Movement;
+import gov.nasa.curiosity.domain.model.Position;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -55,5 +57,149 @@ public class MovementTest {
             Assert.assertEquals("'A' is not a recognized movement. Consider just these: (L,R,M)", ex.getMessage());
             throw ex;
         }
+    }
+
+    @Test
+    public void shouldMoveToLeftFromNorth() {
+        final Position position = Movement.LEFT.move(Position.of(0, 0, Direction.NORTH));
+
+        Assert.assertEquals(0, position.getX());
+        Assert.assertEquals(0, position.getY());
+        Assert.assertEquals(Direction.WEST, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveToRightFromNorth() {
+        final Position position = Movement.RIGHT.move(Position.of(2, 5, Direction.NORTH));
+
+        Assert.assertEquals(2, position.getX());
+        Assert.assertEquals(5, position.getY());
+        Assert.assertEquals(Direction.EAST, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveForwardFromNorth() {
+        final Position position = Movement.MOVE.move(Position.of(0, 0, Direction.NORTH));
+
+        Assert.assertEquals(0, position.getX());
+        Assert.assertEquals(1, position.getY());
+        Assert.assertEquals(Direction.NORTH, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveForwardFromNorthWithNegativeCoordinates() {
+        final Position position = Movement.MOVE.move(Position.of(-1, -1, Direction.NORTH));
+
+        Assert.assertEquals(-1, position.getX());
+        Assert.assertEquals(0, position.getY());
+        Assert.assertEquals(Direction.NORTH, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveToLeftFromEast() {
+        final Position position = Movement.LEFT.move(Position.of(20, 2, Direction.EAST));
+
+        Assert.assertEquals(20, position.getX());
+        Assert.assertEquals(2, position.getY());
+        Assert.assertEquals(Direction.NORTH, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveToRightFromEast() {
+        final Position position = Movement.RIGHT.move(Position.of(7, 9, Direction.EAST));
+
+        Assert.assertEquals(7, position.getX());
+        Assert.assertEquals(9, position.getY());
+        Assert.assertEquals(Direction.SOUTH, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveForwardFromEast() {
+        final Position position = Movement.MOVE.move(Position.of(5, 10, Direction.EAST));
+
+        Assert.assertEquals(6, position.getX());
+        Assert.assertEquals(10, position.getY());
+        Assert.assertEquals(Direction.EAST, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveForwardFromEastWithNegativeCoordinates() {
+        final Position position = Movement.MOVE.move(Position.of(-5, -10, Direction.EAST));
+
+        Assert.assertEquals(-4, position.getX());
+        Assert.assertEquals(-10, position.getY());
+        Assert.assertEquals(Direction.EAST, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveToLeftFromSouth() {
+        final Position position = Movement.LEFT.move(Position.of(15, 25, Direction.SOUTH));
+
+        Assert.assertEquals(15, position.getX());
+        Assert.assertEquals(25, position.getY());
+        Assert.assertEquals(Direction.EAST, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveToRightFromSouth() {
+        final Position position = Movement.RIGHT.move(Position.of(99, 999, Direction.SOUTH));
+
+        Assert.assertEquals(99, position.getX());
+        Assert.assertEquals(999, position.getY());
+        Assert.assertEquals(Direction.WEST, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveForwardFromSouth() {
+        final Position position = Movement.MOVE.move(Position.of(3, 2, Direction.SOUTH));
+
+        Assert.assertEquals(3, position.getX());
+        Assert.assertEquals(1, position.getY());
+        Assert.assertEquals(Direction.SOUTH, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveForwardFromSouthWithNegativeCoordinates() {
+        final Position position = Movement.MOVE.move(Position.of(-3, -2, Direction.SOUTH));
+
+        Assert.assertEquals(-3, position.getX());
+        Assert.assertEquals(-3, position.getY());
+        Assert.assertEquals(Direction.SOUTH, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveToLeftFromWest() {
+        final Position position = Movement.LEFT.move(Position.of(17, 33, Direction.WEST));
+
+        Assert.assertEquals(17, position.getX());
+        Assert.assertEquals(33, position.getY());
+        Assert.assertEquals(Direction.SOUTH, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveToRightFromWest() {
+        final Position position = Movement.RIGHT.move(Position.of(56, 78, Direction.WEST));
+
+        Assert.assertEquals(56, position.getX());
+        Assert.assertEquals(78, position.getY());
+        Assert.assertEquals(Direction.NORTH, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveForwardFromWest() {
+        final Position position = Movement.MOVE.move(Position.of(3, 4, Direction.WEST));
+
+        Assert.assertEquals(2, position.getX());
+        Assert.assertEquals(4, position.getY());
+        Assert.assertEquals(Direction.WEST, position.getDirection());
+    }
+
+    @Test
+    public void shouldMoveForwardFromWestWithNegativeCoordinates() {
+        final Position position = Movement.MOVE.move(Position.of(-3, -4, Direction.WEST));
+
+        Assert.assertEquals(-4, position.getX());
+        Assert.assertEquals(-4, position.getY());
+        Assert.assertEquals(Direction.WEST, position.getDirection());
     }
 }
